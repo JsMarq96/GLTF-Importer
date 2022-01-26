339678d1287b276ad6d93764bba9940a4cf0107f
Juan S. Marquerie <juansebastian.marquerie01@estudiant.upf.edu>
2022-01-26 16:56:53 +0100

    Started work with tinigltf

---
+++ src/gltf_scene.h
@@ -0,0 +1,20 @@
+#ifndef GLTF_SCENE_H_
+#define GLTF_SCENE_H_
+
+#include "gl3w.h"
+#include "glcorearb.h"
+#include "shader.h"
+#include "raw_shaders.h"
+#include "material.h"
+#include "camera.h"
+
+#include "tiny_gltf.h"
+
+#define MAX_MESH_COUNT 2000
+#define MAX_NODE_COUNT 2000
+
+struct sScene {
+
+};
+
+#endif // GLTF_SCENE_H_
